fb8af409c03e6e9e970048575801e040c36b28bc
cdeel <carson.deel@gmail.com>
2024-05-25 10:35:41 -0400

    header

--- ruler.cpp
+++ ruler.cpp
@@ -1,2 +1,8 @@
-#include <Wire.h>
-#include <LiquidCrystal_I2C.h>
+#include "main.h" // Include your main project header
+#include "stm32f4xx_hal.h" 
+#include "lcd_i2c.h" 
+
+#define TRIG_PIN GPIO_PIN_12
+#define TRIG_PORT GPIOC
+#define ECHO_PIN GPIO_PIN_13
+#define ECHO_PORT GPIOC
